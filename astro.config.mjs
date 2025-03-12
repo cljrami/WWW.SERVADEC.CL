3b18ce7f276694a7ee6186b217b04778624139a4
cljrami <cljrami@gmail.com>
2025-03-12 01:08:28 -0300

    a2

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,16 +9,19 @@ import sitemap from '@astrojs/sitemap';
 
 import robotsTxt from 'astro-robots-txt';
 
+import partytown from '@astrojs/partytown';
+
+import metaTags from 'astro-meta-tags';
+
+import pageInsight from 'astro-page-insight';
+
 // https://astro.build/config
 export default defineConfig({
 outDir: './public_html',
+site: 'https://www.servadec.cl',
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [react(), sitemap(), robotsTxt(),
-
-
-
-  ]
+  integrations: [react(), sitemap(), robotsTxt(), partytown(), metaTags(), pageInsight()]
 });
